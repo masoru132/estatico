419fb66023fd0ae4d1954cbff4dbb53732a4a4e2
Masoru <mariadelasoleda.rubio094@comunidadunir.net>
2025-09-06 20:12:17 +0200

    cambios en el sitio

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,11 @@
 const h1 = document.querySelector("h1");
+const body = document.querySelector("body");
 
 function color() {
     return `rgb(${[0, 0, 0].map(() => Math.floor(Math.random() * 256)).join(",")})`;
 }
+
+body.style.backgroundColor = color();
 
 h1.innerHTML = h1.innerText.split("").map(c => {
     return `<span style= "color: ${color()}">${c}</span>`;
